fa27fd82957a5baef39ef5e6092f74607023d0a2
heerens <alexander.heerens@gmail.com>
2024-12-09 09:38:04 +0100

    Test only one Spring app on 3000

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,12 @@
 ENVIRONMENT="prod"
 
 # Start the Spring Boot applications
-java -jar -Dspring.profiles.active=$ENVIRONMENT /app-discover.jar &
+# java -jar -Dspring.profiles.active=$ENVIRONMENT /app-discover.jar &
 # java -jar -Dspring.profiles.active=$ENVIRONMENT /app-checkout.jar &
 
 # Start NGINX
-echo "Start NGINX (nginx-$ENVIRONMENT.conf)..."
-nginx -c /etc/nginx/nginx-$ENVIRONMENT.conf -g "daemon off;"
+# echo "Start NGINX (nginx-$ENVIRONMENT.conf)..."
+#nginx -c /etc/nginx/nginx-$ENVIRONMENT.conf -g "daemon off;"
+
+echo "Start discover on 3000"
+java -jar -Dspring.profiles.active=$ENVIRONMENT /app-discover.jar
