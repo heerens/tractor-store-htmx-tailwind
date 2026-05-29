333e30a7bb7f22e037d821f8500e62c0d2865ed7
heerens <alexander.heerens@gmail.com>
2026-05-29 14:50:00 +0200

    Add git hash version to footer

--- discover/modules/navigation/src/main/kotlin/com/inauditech/tractorstore/navigation/presentation/navigation/footer/Footer.kt
+++ discover/modules/navigation/src/main/kotlin/com/inauditech/tractorstore/navigation/presentation/navigation/footer/Footer.kt
@@ -1,14 +1,20 @@
 package com.inauditech.tractorstore.navigation.presentation.navigation.header
 
 import mu.KLogging
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
 import org.springframework.ui.ModelMap
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class Footer {
+class Footer(
+    @Value("\${app.git-sha:local}") val gitSha: String,
+) {
     @GetMapping("/navigation/fragments/v1/footer")
-    fun home(model: ModelMap): String = "footer/Footer"
+    fun home(model: ModelMap): String {
+        model.addAttribute("gitSha", gitSha)
+        return "footer/Footer"
+    }
 
     companion object : KLogging()
 }
